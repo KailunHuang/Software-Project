5221caf670d8d3177a435eef8cd065df1475a289
Kailun Huang <k.huang20@student.unimelb.edu.au>
2020-04-13 22:40:22 +1000

    finish the design part of configuration page

--- src/application/Configuration_2.java
+++ src/application/Configuration_2.java
@@ -1,16 +1,15 @@
 package application;
-	
+
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -20,12 +19,10 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-
-public class Main extends Application {
-	
+public class Configuration_2 extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -49,18 +46,15 @@ public class Main extends Application {
 			ToggleButton tb3 = new ToggleButton("Cyclist");
 			tb3.setToggleGroup(group);
 			
-			ArrayList<Control> togbutton_controls = new ArrayList<Control>();
-			togbutton_controls.add(tb1);
-			togbutton_controls.add(tb2);
-			togbutton_controls.add(tb3);
-			HBox hbox = addHBox(togbutton_controls);
+			HBox hbox = new HBox(tb1, tb2, tb3);
+			hbox.setPadding(new Insets(0,0,0,25));
 			
 			// The text input sections
 			GridPane grid = new GridPane();
 			grid.setAlignment(Pos.CENTER);
 			grid.setHgap(10);
 			grid.setVgap(10);
-			grid.setPadding(new Insets(25, 25, 25, 25));
+			grid.setPadding(new Insets(25, 25, 50, 25));
 			
 			Label CarType = new Label("Car type:");
 			grid.add(CarType, 0, 0);
@@ -76,26 +70,27 @@ public class Main extends Application {
 			grid.add(Gender, 0, 2);
 			
 			final ToggleGroup group2 = new ToggleGroup();
-			ArrayList<Control> gender_rb = new ArrayList<Control>();
 			RadioButton rb1 = new RadioButton("Male");
 			rb1.setPadding(new Insets(0,10,0,0));
 			rb1.setToggleGroup(group2);
 			rb1.setSelected(true);
-			gender_rb.add(rb1);
+;
 			RadioButton rb2 = new RadioButton("Female");
 			rb2.setPadding(new Insets(0,10,0,0));
 			rb2.setToggleGroup(group2);
-			gender_rb.add(rb2);
-			HBox gender_button = addHBox(gender_rb);
+
+			HBox gender_button = new HBox(rb1, rb2);
 			grid.add(gender_button, 1, 2);
 			
 			//save and input buttons 
-			Button save_button = new Button("save");
-			Button add_button = new Button("Button");
+			Button save_button = new Button("Save");
+			Button add_button = new Button("Add");
 			
+			VBox save_add = new VBox(save_button, add_button);
+			save_add.setPadding(new Insets(25,25,25,25));
 			
-			VBox addBox = vLayoutBox(hbox);
-			addBox.getChildren().addAll(grid, save_button, add_button);
+			VBox addBox = new VBox(hbox, grid, save_add);
+			addBox.setPadding(new Insets(15,15,15,15));
 		
 			
 			//***************************************
@@ -113,16 +108,12 @@ public class Main extends Application {
 	        
 	        table.getColumns().addAll(TrafficCol, GenderCol, AgeCol);
 	        
-	        
-	        ArrayList<Control> view_controls = new ArrayList<Control>();
-	        view_controls.add(table);
-	        
-			VBox viewBox = addVBox(view_controls);
+			VBox viewBox = new VBox(table);
+			viewBox.setPadding(new Insets(15,15,15,15));
 			//***************************************
 			
 			root.setLeft(addBox);
 			root.setRight(viewBox);
-			
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -132,40 +123,10 @@ public class Main extends Application {
 		}
 	}
 	
-	public VBox addVBox(ArrayList<Control> togbutton_controls) {
-		VBox vbox = new VBox();
-		vbox.setPadding(new Insets(10));
-		
-		
-		for (int i = 0; i < togbutton_controls.size(); i++) {
-			vbox.getChildren().add(togbutton_controls.get(i));
-		}
-		
-		return vbox;
-		
-	}
-	
-	public VBox vLayoutBox(Pane pane) {
-		VBox vbox = new VBox();
-		vbox.setPadding(new Insets(10));
-        
-        vbox.getChildren().add(pane);
-		return vbox;
-		
-	}
-	
-	public HBox addHBox(ArrayList<Control> togbutton_controls) {
-		HBox hbox = new HBox();
-		
-		for (int i = 0; i < togbutton_controls.size(); i++) {
-			hbox.getChildren().add(togbutton_controls.get(i));
-		}
-		return hbox;
-		
-	}
+
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
-
--- src/application/welcomePage.java
+++ src/application/welcomePage.java
@@ -1,56 +1,35 @@
-package roadSimulation;
+package application;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class welcomePage {
-	private static JFrame welframe = new JFrame("welcome");
-	private static Container c = welframe.getContentPane();
-	private static JLabel welLable = new JLabel("username");
-	private static JButton newSimulation = new JButton("New Simulation");
-	private static JButton load = new JButton("Load Simulation File");
-	public welcomePage() {
+import java.awt.Label;
+import java.net.URL;
+
+public class welcomePage extends Application{
 	
-	welframe.setBounds(600, 200, 300, 220);
-	     
-	c.setLayout(new BorderLayout());
-    welframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      
-	        initial();
-	       
-    welframe.setVisible(true);
-	     }
-	public void initial() {
-	    JPanel titlePanel = new JPanel();
-        titlePanel.setLayout(new FlowLayout());
-	         JPanel button = new JPanel();
-	        button.setLayout(new FlowLayout());
-	      button.add(newSimulation);
-	        button.add(load);
-	         c.add(button);
-	   }
-	     public static void main(String[] a) {
-	        welcomePage w=new welcomePage();
-	        ActionListener bb=new ActionListener() {
-	        	public void actionPerformed(ActionEvent arg0) {
-	        		configure mm=new configure();
-	        		w.welframe.dispose();
-	        	}
-	        };
-	        ActionListener cc=new ActionListener() {
-	        	public void actionPerformed(ActionEvent b) {
-	        		overview nn=new overview();
-	        		w.welframe.dispose();
-	        	}
-	        };
-	        newSimulation.addActionListener(bb);
-	        load.addActionListener(cc);
-	     } 
+	@FXML public Label label1;
+	
+	@FXML public Button button1;
+	
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+//    	Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+    	VBox root = (VBox) FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+    	
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
