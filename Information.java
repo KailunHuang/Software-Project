64843a093f271fc92c0ac90535546ce37510b092
Zhong Lintong <lintong.zhong@student.unimelb.edu.au>
2020-04-15 18:47:14 +1000

    table function added

---
+++ Information.java
@@ -0,0 +1,37 @@
+package roadSimulation;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Information {
+	private final SimpleStringProperty cartype;
+	private final SimpleStringProperty age;
+	private final SimpleStringProperty gender;
+	
+	
+	public Information(String ctype, String ae, String gder) {
+		this.cartype=new SimpleStringProperty(ctype);
+		this.age=new SimpleStringProperty(ae);
+		
+		this.gender=new SimpleStringProperty(gder);
+	}
+	
+	public String getCartype() {
+		return cartype.get();
+	}
+	public void setCartype(String ctype) {
+		cartype.set(ctype);;
+	}
+	public String getAge(){
+		return age.get();
+	}
+	public void setAge(String ae) {
+		age.set(ae);
+	}
+	public String getGender(){
+		
+		return gender.get();
+	}
+	public void setGender(String gder) {
+		age.set(gder);;
+	}
+}
