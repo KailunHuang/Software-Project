a8a0b5b08bb4228235948d45d7040188ed5b192f
JasonLuo719 <903587279@qq.com>
2020-04-30 20:53:21 +0800

    20200430 solve conflicts

--- src/application/Car.java
+++ src/application/Car.java
@@ -3,13 +3,11 @@ package application;
 import java.util.Random;
 
 public class Car extends Traffic {
-<<<<<<< HEAD
 	//private String Type;
 	
-	public Car(String type, Boolean gender, int age) {
+	public Car(Boolean gender, int age) {
+		super(gender,age);
 		//this.Type = type;
-		this.is_Male = gender;
-		this.age = age;
 		this.carelessRate = 0.7 - 0.1*age*70;
 		if(this.carelessRate<0){
 			carelessRate = 0;
@@ -22,15 +20,6 @@ public class Car extends Traffic {
 			this.payoff_crash -= 10;
 			this.payoff_pass += 10;
 		}
-=======
-	public String Type;
-
-	
-	public Car(String type, Boolean gender, int age) {
-		super(gender, age);
-		super.V_type="Car";
-		this.Type = type;
->>>>>>> 815d809a4a3738d0aa5959e36160081af94d5095
 	}
 
 }
